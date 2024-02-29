0f584e9fd22a3b547d73c3a9ca73bfd2e06bddf7
kenneth <nwadiaro101charles231@gmail.com>
2024-02-29 12:39:03 -0500

    modify main.tf provider.tf , create variable.tf

---
+++ provider.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "SG1" {
+  name = "Terraform-ASG-SG"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
